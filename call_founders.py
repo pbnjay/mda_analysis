fb20f0e53a598e81fb892ea7acd97a7d9a1abf08
Jeremy Jay <jeremy@pbnjay.com>
2012-01-22 21:40:31 -0500

    add a 2nd phase to improve accuracy

--- call_founders.py
+++ call_founders.py
@@ -10,14 +10,26 @@
 # direction, repeat it in the backward direction. (if you ever come to a
 # heterozygous call, skip it).
 #
-# Once this is completed, walk down the chromosome again, and output any
+# In a second phase, repeat the forward walk again, this time counting
+# SNPs with a single founder, aggregated by founder. (for this we use a
+# sliding window of N=1500 SNPs) When the walk reaches a SNP which still
+# have an ambiguous founder (ie 3 possible founders), find the founder
+# which has been seen the most in the last N singleton-founder SNPs, and
+# save it. Repeat this process in the backwards direction, replacing
+# "best" founders if they have been seen in more of the last N SNPs than
+# the forward direction. Then, walk through the saved maps and replace
+# the founder calls.
+#
+# Once these is completed, walk down the chromosome again, and output any
 # (homozygous) snps that have a single founder attributed. Also
 # calculate the error rate of the determinations.
+#
 
 from collections import deque
 import sys
 
-windowsize=250
+windowsize=25
+previous_max=1500
 
 # read in the file and parse header
 f = open(sys.argv[1])
@@ -60,9 +72,12 @@ for line in f:
             elif d[idx][0]==b[0][0]: # homozygous B
                 snpinfo['calls'][idx-1] = (1, set(b[1]), b[1])
                 hasmatch=True
+            elif d[idx][0]=='-': # no call made
+                snpinfo['calls'][idx-1] = (0,)
             else:
+                print 'Homozygous, but not AA/BB : %s -- %s, %s' %(d[idx], a,b)
                 # wtf?
-                snpinfo['calls'][idx-1] = (2, set(),set(), frozenset(),frozenset())
+                snpinfo['calls'][idx-1] = (0, set(),set(), frozenset(),frozenset())
 
         # verify it is a het call
         elif (d[idx][0]==a[0][0] and d[idx][1]==b[0][0]) or (d[idx][1]==a[0][0] and d[idx][0]==b[0][0]):
@@ -70,7 +85,8 @@ for line in f:
             hasmatch=True
 
         else:
-            snpinfo['calls'][idx-1] = (2, set(), set(), frozenset(),frozenset())
+            print 'Unknown call : %s -- %s, %s' %(d[idx], a,b)
+            snpinfo['calls'][idx-1] = (0, set(), set(), frozenset(),frozenset())
             continue
 
     if hasmatch:
@@ -81,18 +97,59 @@ for line in f:
 f.close()
 print "%d SNPs loaded." % (len(allsnps),)
 
-def _process_chr(snpset, idx, with_other=None):
-    last = None
-    for pos,snp in snpset:
-        if allsnps[snp][idx][0]!=1:
-            continue
+def _process_chr(thesnps, idx):
+    for snpset in [sorted(thesnps), sorted(thesnps, reverse=True)]:
+        last = None
+        for pos,snp in snpset:
+            if allsnps[snp][idx][0]!=1:
+                continue
 
-        if last is not None:
-            if not allsnps[last[1]][idx][1].isdisjoint( allsnps[snp][idx][1] ):
-                allsnps[last[1]][idx][1].intersection_update( allsnps[snp][idx][1] )
-                allsnps[snp][idx][1].intersection_update( allsnps[last[1]][idx][1] )
+            if last is not None:
+                if not allsnps[last[1]][idx][1].isdisjoint( allsnps[snp][idx][1] ):
+                    allsnps[last[1]][idx][1].intersection_update( allsnps[snp][idx][1] )
+                    allsnps[snp][idx][1].intersection_update( allsnps[last[1]][idx][1] )
 
-        last = pos,snp
+            last = pos,snp
+
+def _process_chr2(thesnps, idx):
+    best_founders = {}
+
+    for snpset in [sorted(thesnps), sorted(thesnps, reverse=True)]:
+        previous_founders = deque()
+        recent_counts = {}
+
+        for pos,snp in snpset:
+            if allsnps[snp][idx][0]!=1:
+                continue
+
+            if len(allsnps[snp][idx][1])==1:
+                for fdr in allsnps[snp][idx][1]:
+                    if fdr not in recent_counts:
+                        recent_counts[ fdr ] = 1
+                    else:
+                        recent_counts[ fdr ] += 1
+                    previous_founders.append( fdr )
+
+                if len(previous_founders)==previous_max:
+                    recent_counts[ previous_founders.popleft() ] -= 1
+
+            else:
+                best_num = 0
+                best = None
+                for fdr in allsnps[snp][idx][1]:
+                    if fdr in recent_counts and recent_counts[fdr]>best_num:
+                        best=fdr
+                        best_num=recent_counts[fdr]
+
+                if best is not None:
+                    if snp not in best_founders:
+                        best_founders[snp] = (best, best_num)
+                    elif best_founders[snp][1]<best_num:
+                        best_founders[snp] = (best, best_num)
+
+    for snp in best_founders:
+        allsnps[snp][idx][1].intersection_update( [best_founders[snp][0]] )
+
 
 fout = open(sys.argv[2], 'w')
 fout2 = open(sys.argv[3], 'w')
@@ -100,10 +157,11 @@ print >>fout, "SNP ID\tchr\tposition\t%s" % ("\t".join(header[1:num_arrays+1]), 
 print >>fout2, "SNP ID\tchr\tposition\t%s" % ("\t".join(header[1:num_arrays+1]), )
 
 for chr,snpset in chrsnps.items():
+    chrscore = [0.0,0]
     print "  Chr"+chr
     for idx in range(0,num_arrays):
-        _process_chr(sorted(snpset),idx)
-        _process_chr(sorted(snpset,reverse=True),idx)
+        _process_chr(snpset,idx)
+        _process_chr2(snpset,idx)
 
     # output our founder calls and accuracy metric
     lastfoundercall=None
@@ -117,7 +175,7 @@ for chr,snpset in chrsnps.items():
                 chrfounders.append(' // '.join([f,f]))
                 goods+=1
 
-            #elif allsnps[snp][idx][0]==2:
+            #elif allsnps[snp][idx][0]==1 or allsnps[snp][idx][0]==2:
             #    chrfounders.append( ' // '.join(allsnps[snp][idx][1]) +' /// '+ ' // '.join(allsnps[snp][idx][2]) )
             #    goods+=1
             #else:
@@ -134,26 +192,38 @@ for chr,snpset in chrsnps.items():
 
         curscores = []
         for idx in range(0,num_arrays):
-            score=0.0
-            fdrs = lastfoundercall[idx].split(' // ')
-            tc=[-1,-1]
-            if allsnps[snp][idx][0]==2:
-                tc[0]=-2
-            if fdrs[0] in allsnps[snp][idx][ tc[0] ]:
-                score+=0.5
-            if fdrs[1] in allsnps[snp][idx][ tc[1] ]:
-                score+=0.5
 
-            scores[idx].append(score)
-            if len(scores[idx])>windowsize:
-                scores[idx].popleft()
+            # do not score no-calls
+            if allsnps[snp][idx][0]>0:
+                score=0.0
+                tc=[-1,-1]
+
+                if allsnps[snp][idx][0]==2:
+                    tc[0]=-2
+                fdrs = lastfoundercall[idx].split(' // ')
+                if fdrs[0] in allsnps[snp][idx][ tc[0] ]:
+                    score+=0.5
+                if fdrs[1] in allsnps[snp][idx][ tc[1] ]:
+                    score+=0.5
+
+                scores[idx].append(score)
+                if len(scores[idx])>windowsize:
+                    scores[idx].popleft()
+                chrscore[0]+=score
+                chrscore[1]+=1
 
             sum=0.0
             for score in scores[idx]:
                 sum+=score
-            curscores.append("%5.4f" % (float(sum) / float(len(scores[idx]))))
+            sum = float(sum) / float(len(scores[idx]))
+            curscores.append("%4.3f // %4.3f" % (sum,sum))
 
         print >>fout2, "%s\t%s\t%s\t%s" % (snp,chr,pos, '\t'.join(curscores))
+
+    try:
+        print "    Chr %s Accuracy: %4.3f" % (chr, chrscore[0]/float(chrscore[1]),)
+    except:
+        print "    Chr %s Accuracy: 0.000" % (chr,)
 
 fout.close()
 fout2.close()
